f8ad4d37e3b9e339dafdb020021c6d19676d97f0
Pablo <pablomd_98@hotmail.com>
2024-08-29 21:59:16 -0300

    se elimina todo lo que estaba desarrollado en esta rama, para poder trabajar mas organizado

--- db/configdb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-try {
-    mongoose.connect(process.env.MONGODB_CONNECT).then(() => console.log('Base de datos conectada'));
-} catch (error) {
-    console.log(error);
-};
-
-module.exports = mongoose;
--- models/pregunta.schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PreguntaSchema = new mongoose.Schema({
-    contenido: String,
-    tipo: String,
-});
-
-const PreguntaModel = mongoose.model('pregunta', PreguntaSchema);
-module.exports = PreguntaModel;
