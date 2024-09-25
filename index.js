0addc46924f89134ae535e1b45897688d22c65b8
Pablo <pablomd_98@hotmail.com>
2024-09-25 18:35:26 -0300

    se elimina controllers, models y services para poder iniciar de nuevo

--- index.js
+++ index.js
@@ -1,66 +1,21 @@
 require('dotenv').config();
-const Server = require('./server/config');
-const server = new Server();
+const { Configuration, OpenAIApi } = require('openai');
 
-server.listen();
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
+async function testOpenAI() {
+    try {
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: '¿que es un QA?' }],
+        });
+        console.log(response.data.choices[0].message.content);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
 
-
-
-// Esto no sirve 
-
-
-// const http = require('http');
-// const url = require('url');
-
-// let preguntas = {};
-
-// const router = {
-//     GET: {},
-//     POST: {}
-// };
-
-// const handlerRoute = (metodo, ruta, funcion) => {
-//     router[metodo]
-// };
-
-// handlerRoute('GET', '/preguntas', (req, res) => {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end(JSON.stringify(preguntas));
-// });
-
-// handlerRoute('POST', '/preguntas', (req, res) => {
-//     let body = '';
-
-//     req.on('data', (fragamentosDelBody) => {
-//         body += fragamentosDelBody.toString();
-//     });
-
-//     req.on('end', () => {
-//         const pregunta = JSON.parse(body);
-//         const id = crypto.randomUUID();
-
-//         preguntas[id] = {id, pregunta};
-//         res.writeHead(201, { 'Content-Type': 'text/plain' });
-//         res.end(JSON.stringify(preguntas));
-//     });
-   
-// });
-
-// const server = http.createServer((req, res) => {
-//     const urlPrueba = url.parse(req.url, true)
-//     const metodo = req.method
-//     const ruta = urlPrueba.pathname
-//     const funcion = router[metodo][ruta]
-//     // res.end()
-//     if (funcion) {
-//         funcion(req, res)
-//     } else {
-//         res.writeHead(404, { 'Content-Type': 'text/plain' })
-//         res.end('Error en la ruta')
-//     }
-// });
-
-// server.listen(3000, () => {
-//     console.log('Servidor funcionando')
-// });
+testOpenAI();
